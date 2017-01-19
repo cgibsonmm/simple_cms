500acc6aa0f5f097cc3cf97fd23dd8aa766e089a
Corey Gibson <cgibsonmm@gmail.com>
2017-01-19 18:52:37 -0500

    Finished chapter 3

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,8 +1,12 @@
 class DemoController < ApplicationController
 
   def index
+    render 'index'
   end
 
   def hello
+    @array = (1..5).to_a
+    @id = params['id']
+    @page = params[:page]
   end
 end
