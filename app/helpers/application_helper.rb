4a7422fa4436810c4421110374dcf0874193cbef
cgibsonmm <cgibsonmm@gmail.com>
2017-01-19 03:01:19 -0500

    added dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # provide title on page basis
+  def full_title(title = '')
+    base_title = 'SimpleCms'
+    if title.empty?
+      base_title
+    else
+      base_title + " | " + title
+    end
+  end
 end
--- test/controllers/demo_controller_test.rb
+++ test/controllers/demo_controller_test.rb
@@ -4,10 +4,12 @@ class DemoControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get demo_index_url
     assert_response :success
+    assert_select "title", "SimpleCms | Index"
   end
 
-  test "Should get about" do
-    get demo_about_url
+  test "Should get hello" do
+    get demo_hello_url
     assert_response :success
+    assert_select 'title', 'SimpleCms | Hello'
   end
 end
