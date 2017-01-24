ba0a3d2e5272c85f38074376033e8b5308764568
Corey Gibson <cgibsonmm@gmail.com>
2017-01-23 23:57:59 -0500

    created admin_user_pages join table

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,6 @@
 class AdminUser < ApplicationRecord
   # When changing table names must change pre-gen file names or points
   # self.table_name = "admin_users"
+
+  has_and_belongs_to_many :pages
 end
