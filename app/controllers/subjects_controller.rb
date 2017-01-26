702d4a4b602eaf6b6d1191abb0376bf5a6b81c21
Corey Gibson <cgibsonmm@gmail.com>
2017-01-26 00:28:12 -0500

    added new and show to sujects controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,14 +1,19 @@
 class SubjectsController < ApplicationController
   def index
+    @subjects = Subject.sorted
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new(name: 'Name')
   end
 
   def create
+    @subject = Subject.new(params[:subject])
+    @subject.save ? redirect_to(subjects_path) : render('new')
   end
 
   def edit
