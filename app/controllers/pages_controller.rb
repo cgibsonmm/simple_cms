b1aea7e9be64ef2d45900546a24e4874874a6d0a
Corey Gibson <cgibsonmm@gmail.com>
2017-01-26 19:15:17 -0500

    Finished pages and sections controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,54 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+    @page.subject_id = 9
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      flash[:notice] = "Page updated"
+      redirect_to(pages_path)
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @page = Page.find(params[:id])
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    flash[:notice] = 'Page deleted'
+    redirect_to(pages_path)
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:subject_id, :name, :permalink, :position, :visable)
+  end
+end
---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,54 @@
+class SectionsController < ApplicationController
+  def index
+    @sections = Section.all
+  end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def new
+    @section = Section.new
+  end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      flash[:notice] = "New section created"
+      redirect_to(sections_path)
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    if @section.update(section_params)
+      flash[:notice] = "Section Update"
+      redirect_to(section_path(@section))
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @section = Section.find(params[:id])
+  end
+
+  def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    flash[:notice] = 'Section Deleted'
+    redirect_to(sections_path)
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:page_id, :name, :position, :content_type, :content)
+  end
+end
