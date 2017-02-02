8ee931e920ac7a994a592f40d7004451f4b5579b
cgibsonmm <cgibsonmm@gmail.com>
2017-02-02 09:02:26 -0500

    added extra styles to project

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -5,5 +5,7 @@ class Subject < ApplicationRecord
   scope :invisible, -> { where(visible: false) }
   scope :sorted, -> { order('position ASC') }
   scope :newest_first, -> { order('created_at DESC') }
-  scope :search, lambda {|query| where("name LIKE ?", "%#{query}%")}
+  scope :search, lambda { |query| where('name LIKE ?', "%#{query}%") }
+
+  validates_presence_of :name
 end
