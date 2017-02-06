4ab0f70d6f8a27f86fa64c7b8951b6a44329c994
Corey Gibson <cgibsonmm@gmail.com>
2017-02-06 11:19:39 -0500

    Added login logout admin

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,39 @@
 class AccessController < ApplicationController
+
+  layout 'admin'
+
+
+  before_action :confirm_logged_in, except: [:login, :attempt_login, :logout]
+
   def menu
+    # displays tex and link
   end
 
   def login
+  end
+
+  def attempt_login
+    if params[:user_name].present? && params[:password].present?
+      found_user = AdminUser.where(user_name: params[:user_name]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if authorized_user
+      session[:user_id] = authorized_user.id
+      flash[:notice] = "You are now logged in"
+      puts "Logged in *********"
+      redirect_to(admin_path)
+    else
+      flash.now[:notice] = 'invalid username/password combination.'
+      render('login')
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "logged out"
+    redirect_to(access_login_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'access/menu'
-
-  get 'access/login'
 
   root 'demo#index'
+
+  get 'admin', to: 'access#menu'
+  get 'access/menu'
+  get 'access/login'
+  post 'access/attempt_login'
+  get 'access/logout'
+
 
   resources :subjects do
     member do
